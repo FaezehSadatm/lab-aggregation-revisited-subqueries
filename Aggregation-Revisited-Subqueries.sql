3b209ccefd77cdb2d8ba9cbea312d10586522ba8
FaezehSadat <faezehsadatm@gmail.com>
2023-02-03 14:09:36 +0100

    lab-aggregation-revisited-subqueries

--- Aggregation-Revisited-Subqueries.sql
+++ Aggregation-Revisited-Subqueries.sql
@@ -38,14 +38,16 @@ where c.customer_id in (
 	);
  
  # Verify if the above two queries produce the same results or not.
-# Yes, I've received the same results.
+-- I've received the same results.
  
  
 # Use the case statement to create a new column classifying existing columns as either or high value transactions based on the amount of payment. If the amount is between 0 and 2, label should be low and if the amount is between 2 and 4, the label should be medium, and if it is more than 4, then it should be high.
-select *, 
-	case 
-		when amount < 2 then "low" 
-        when amount >= 2 and amount < 4 then "medium"
-        when amount >= 4 then "high"
-    end as value_transactions
-from payment;
+select c.customer_id, concat(c.first_name, " ", c.last_name) as name, round(avg(p.amount), 2) as avg_amount, 
+	case
+		when round(avg(p.amount), 2) < 2 then 'low'
+		when round(avg(p.amount), 2) >= 2 and round(avg(p.amount), 2) < 4 then 'medium'
+		when round(avg(p.amount), 2) >= 4 then 'high'
+	 end as value_transaction
+from customer c
+join payment p on c.customer_id = p.customer_id 
+group by c.customer_id;
